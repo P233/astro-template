27a7b51c5a1fe1c89097afc7ad2874fee388167a
Peiwen Lu <hi@peiwen.lu>
2022-10-08 22:47:14 +0800

    Enable Prettier

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 120,
+  trailingComma: "none",
+  plugins: [require.resolve("prettier-plugin-astro")],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
