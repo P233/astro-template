1cb1fd119abb1ee1e5f4cef151cd2634649c80e6
Peiwen Lu <hi@peiwen.lu>
2023-10-01 16:58:31 +0800

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,49 @@
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
 import { defineConfig } from "astro/config";
 import compress from "astro-compress";
+import autoprefixer from "autoprefixer";
+
+let modulesConfig = {
+  generateScopedName: "[local]-[hash:base64:4]"
+};
+
+if (process.env.IS_PROD) {
+  const fileSet = {};
+  const hashSet = {};
+  modulesConfig = {
+    getJSON: function (file, json) {
+      if (fileSet[file]) return;
+
+      fileSet[file] = true;
+      Object.values(json).forEach((i) => {
+        if (hashSet[i]) throw Error("HASH COLLISION ERROR");
+        hashSet[i] = true;
+      });
+    },
+    generateScopedName: "[hash:base64:2]"
+  };
+}
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress()]
+  integrations: [compress()],
+  vite: {
+    css: {
+      modules: modulesConfig,
+      postcss: {
+        plugins: [autoprefixer()]
+      },
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "~/styles/_common" as *;`
+        }
+      }
+    },
+    resolve: {
+      alias: {
+        "~/": `${path.resolve(dirname(fileURLToPath(import.meta.url)), "src")}/`
+      }
+    }
+  }
 });
